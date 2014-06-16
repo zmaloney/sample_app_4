1843b2a7cb604c1e1985b161ed2fa78c6786dbc7
Zacharie Maloney <zacharie.maloney@gmail.com>
2014-06-16 19:30:55 -0400

    So not all our tests pass, but at least our routes work. That's something.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,12 +4,38 @@ describe "AuthenticationPages" do
   
   subject { page }
   
-  describe "signin_page" do 
+  describe "signin" do 
     before { visit signin_path }
     
-    it { should have_selector('h1',     text: 'Sign in') } 
-    it { should have_selector('title',  text: 'Sign in') }
+    #invalid or no information should show us "sign in" and an error alert.
+    describe "with invalid information" do
+      before { click_button "Sign in" } #submit with no info
+      it { should have_selector('title',  text: 'Sign in') }
     
+      #and ... does invalid info render an error message?
+      # We should see a div with classes "alert" and "alert-error"
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') } 
+    end
+    
+    # How about a success?
+    describe "with valid information" do 
+      let(:user) { FactoryGirl.create(:user) }
+      before do 
+        fill_in "Email",      with: user.email
+        fill_in "Password",   with: user.password
+        click_button "Sign in"
+      end
+      
+      #now, since it was successful, we should have a bunch of info. 
+      # ... a title with the user name in it
+      it { should have_selector('title', text: user.name) }
+      # ... a link to the user profile page
+      it { should have_link('Profile', href: user_path(user)) } 
+      # ... a link to sign out
+      it { should have_link('Sign out', href: signout_path) }
+      # ... and we should no longer show the 'sign in' link
+      it { should_not have_link('Sign in', href: signin_path) } 
+    end 
   end
   
 end
